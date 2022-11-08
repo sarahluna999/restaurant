032da5dd717c061aa7f86740753c84097eea2441
sarahluna999 <117298860+sarahluna999@users.noreply.github.com>
2022-11-08 15:11:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -43,11 +43,6 @@
     }*/
 
 ?>
-<?php  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-   if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-
-    $database = mysqli_select_db($connection, DB_DATABASE);
-?>
 
 	<!-- HOME SECTION -->
 

d2c407cca901b0cc46640f0d854f930dac6d6c68
sarahluna999 <117298860+sarahluna999@users.noreply.github.com>
2022-11-08 14:41:39 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,19 @@
-<html>
-	<body>
 <?php
-
-define('DB_SERVER', 'resto.ck4nkgx3zsuf.us-east-1.rds.amazonaws.com');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'easy1999');
-define('DB_DATABASE', 'restaurant-website');
-
+    $dsn = 'mysql:host=resto.ck4nkgx3zsuf.us-east-1.rds.amazonaws.com;dbname=restaurant_website';
+	$user = 'admin';
+	$pass = 'easy1999';
+	$option = array(
+		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+	);
+	try
+	{
+		$con = new PDO($dsn,$user,$pass,$option);
+		$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		//echo 'Good Very Good !';
+	}
+	catch(PDOException $ex)
+	{
+		echo "Failed to connect with database ! ".$ex->getMessage();
+		die();
+	}
 ?>
-		
-	</body>
-</html>

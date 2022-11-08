382970f9ffa0a7c85fcdb692af5cef00648e5e13
sarahluna999 <117298860+sarahluna999@users.noreply.github.com>
2022-11-08 14:59:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     include 'Includes/functions/functions.php';
     include "Includes/templates/header.php";
     include "Includes/templates/navbar.php";
+	$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+  	$database = mysqli_select_db($connection, DB_DATABASE);
 
 
     //Getting website settings
@@ -49,7 +53,7 @@
 		<div class="container">
 			<div class="row" style="flex-wrap: nowrap;">
 				<div class="col-md-6 home-left-section">
-					<div style="padding: 100px 0px; color: black;">
+					<div style="padding: 100px 0px; color: white;">
 						<h1>
 							RESTAURANT LA ROSA.
 						</h1>

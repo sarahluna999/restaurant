1e34426a6827010f7dd9fcc68a805ce56f617ba5
sarahluna999 <117298860+sarahluna999@users.noreply.github.com>
2022-11-08 13:27:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <!-- PHP INCLUDES -->
+<!DOCTYPE html>
+<html lang="en">
+	
+	<!-- HEAD -->
 
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+		<meta name="author" content="JAIRI IDRISS">
+		<title><?php getTitle(); ?></title>
+
+		<!-- EXTERNAL CSS LINKS -->
+
+		<link rel="stylesheet" type="text/css" href="Design/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="Design/fonts/css/all.min.css">
+		<link rel="stylesheet" type="text/css" href="Design/css/main.css">
+		<link rel="stylesheet" type="text/css" href="Design/css/responsive.css">
+
+		<!-- GOOGLE FONTS -->
+
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
+
+	</head>
+
+	<!-- BODY -->
+
+	<body>
+	
 <?php
 
-    //include "connect.php";
+    include "connect.php";
 	
-    //include 'Includes/functions/functions.php';
-    include "Includes/templates/header.php";
+    include 'Includes/functions/functions.php';
+    //include "Includes/templates/header.php";
     include "Includes/templates/navbar.php";
 
 
@@ -493,7 +522,7 @@
 
     <!-- FOOTER BOTTOM  -->
 
-    <?php include "Includes/templates/footer.php"; ?>
+    
 
     <script type="text/javascript">
 
